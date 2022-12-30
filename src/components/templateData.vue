e2037e8d9ef859ecb8c5bf46729900cac96c7333
whc <whc@wushikj.com>
2022-12-30 14:57:13 +0800

    first: init

---
+++ src/components/templateData.vue
@@ -0,0 +1,14 @@
+<!-- 模板字符 -->
+<template>
+  <div class='templateData'>
+    <input type="text" v-model="value">
+    <div>{{value}}</div>
+  </div>
+</template>
+
+<script setup  lang='ts'>
+  import { ref } from 'vue'
+  const value = ref('text')
+</script>
+<style lang='scss'>
+</style>
